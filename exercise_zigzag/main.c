94c775b09e49d7fcb561a13a11515c877e6c37c1
Adam Albarghouthi <adambargh@gmail.com>
2014-06-21 10:29:21 -0400

    Initial Commit

---
+++ exercise_zigzag/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  exercise_zigzag
+//
+//  Created by Adam Albarghouthi on 2014-06-21.
+//  Copyright (c) 2014 Adam Albarghouthi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
